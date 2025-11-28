5977fbed40db3e04196ec196df6cc1efa9953744
Gabriel-MRodrigues <gabrielmendesrodriguest@hotmail.com>
2025-11-27 20:19:15 -0400

    Controlling the LED with a button

--- LED_BLINK/src/main.cpp
+++ LED_BLINK/src/main.cpp
@@ -1,32 +1,27 @@
 #include <Arduino.h>
-#define LED_YELLOW_PIN 15
-#define DELAY 1000
-#define LED_GREEN_PIN 4
-#define LED_RED_PIN 0
+#define BTN_PIN 15
+#define LED_PIN 12
 
+bool ledState = false;
+bool lastButtonState = LOW;
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(LED_YELLOW_PIN, OUTPUT);
-  pinMode(LED_GREEN_PIN, OUTPUT);
-  pinMode(LED_RED_PIN, OUTPUT);
+  pinMode(LED_PIN, OUTPUT);
+  pinMode(BTN_PIN, INPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(LED_RED_PIN, HIGH);
-  delay(5000);
-  digitalWrite(LED_RED_PIN, LOW);
-  delay(1000);
+  bool currentButtonState = digitalRead(BTN_PIN);
 
-  digitalWrite(LED_YELLOW_PIN, HIGH);
-  delay(2000);
-  digitalWrite(LED_YELLOW_PIN, LOW);
-  delay(1000);
+  if(currentButtonState == HIGH && lastButtonState == LOW) {
+    ledState = !ledState;
+    delay(50);
+  }
 
-  digitalWrite(LED_GREEN_PIN, HIGH);
-  delay(5000);
-  digitalWrite(LED_GREEN_PIN , LOW);
-  delay(1000);
+  lastButtonState = currentButtonState;
+
+  digitalWrite(LED_PIN, ledState ? HIGH : LOW);
 }
 
